b4f59759b73816b735defcd3d0c22bce771c301f
Ngọc Dũng <Phungdung2708@gmail.com>
2025-05-22 20:30:20 +0700

    Master layouts configuration

--- backend/app/Http/Controllers/HomeController.php
+++ backend/app/Http/Controllers/HomeController.php
@@ -7,7 +7,23 @@ use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    public function index(){
+    public function index()
+    {
         return view('index');
+    }
+
+    public function pJM()
+    {
+        return view('dashboard/project-management');
+    }
+
+    public function product()
+    {
+        return view('app/e-commerce/admin/product');
+    }
+
+    public function addProduct()
+    {
+        return view('app/e-commerce/admin/add-product');
     }
 }
