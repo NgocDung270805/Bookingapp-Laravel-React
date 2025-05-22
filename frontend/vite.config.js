a2fbb49ba761f6b330639354979ec8fc6f938f40
Ngọc Dũng <Phungdung2708@gmail.com>
2025-05-22 20:23:36 +0700

    Configure vite to share ngrok ip

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true, // Giữ lại nếu bạn cần truy cập từ IP khác 
+  }
 })
+
+// Cấu hình cho ngrok chạy trên localhost
+// const ngrokHost = 'a0cd-42-118-57-49.ngrok-free.app'
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     host: true, // Cho phép truy cập từ external network
+//     allowedHosts: [ngrokHost], // Thêm domain ngrok vào danh sách cho phép
+//   }
+// })
