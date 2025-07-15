a57b12df58ecf161272e4c4438ef25148c711f7e
Ngọc Dũng <phungdung2708@gmail.com>
2025-07-15 21:23:48 +0700

    FE update(ProductsPages): update Price, View By Products

--- frontend/src/modules/Products/containers/ProductsByCategoriesPage.jsx
+++ frontend/src/modules/Products/containers/ProductsByCategoriesPage.jsx
@@ -2,7 +2,7 @@
 
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { Link, Links, useParams } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 import {
   fetchProducts,
   selectAllProducts,
