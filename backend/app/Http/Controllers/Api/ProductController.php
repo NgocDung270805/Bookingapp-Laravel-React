70ddc24d5d676a162b0cab29c0840ca122c18709
Phùng Ngọc Dũng <phungdung2708@gmail.com>
2025-09-14 16:55:49 +0700

    BE update(api product): update Api Show Products, Favorites, Comments, Bookings

--- backend/app/Http/Controllers/Api/ProductController.php
+++ backend/app/Http/Controllers/Api/ProductController.php
@@ -106,7 +106,7 @@ class ProductController extends Controller
     {
         try {
             $product = Product::where('slug', $product_slug)
-                                ->with('images', 'categories', 'tags', 'variants.attributeValues.attributeType', 'attributeValueConfigs.attributeValue.attributeType') // Đã sửa từ 'category' thành 'categories'
+                                ->with('images', 'categories', 'tags', 'variants.attributeValues.attributeType', 'attributeValueConfigs.attributeValue.attributeType', 'favoritedByUsers', 'comments', 'bookings') 
                                 // ->with('comments.user', 'variants.attributeValues')
                                 ->first();
 
