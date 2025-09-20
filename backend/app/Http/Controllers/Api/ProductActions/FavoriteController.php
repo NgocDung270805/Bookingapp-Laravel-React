5b65b6b351cf33ac0f2c38856bc65671b87b38d4
Phùng Ngọc Dũng <phungdung2708@gmail.com>
2025-09-20 15:44:28 +0700

    BE update(api favorite): update Add Condition to Get Favorite Product Status == 1

--- backend/app/Http/Controllers/Api/ProductActions/FavoriteController.php
+++ backend/app/Http/Controllers/Api/ProductActions/FavoriteController.php
@@ -10,6 +10,21 @@ use Illuminate\Support\Facades\Auth;
 class FavoriteController extends Controller
 {
     /**
+     * Get all favorited products for the authenticated user.
+     */
+    public function index()
+    {
+        $user = Auth::user();
+        // Lấy tất cả sản phẩm yêu thích của người dùng có trạng thái active (status = 1), và trả về variants.attributeValues.attributeType
+        $favoritedProducts = $user->favorites()->where('status', 1)->with('variants.attributeValues.attributeType')->get();
+        return response()->json([
+            'status' => 200,
+            'message' => 'Lấy danh sách sản phẩm yêu thích thành công!',
+            'favorited_products' => $favoritedProducts
+        ]);
+    }
+
+    /**
      * Toggle a product as favorite/unfavorite for the authenticated user.
      */
     public function toggle(Product $product)
@@ -54,20 +69,6 @@ class FavoriteController extends Controller
             'status' => 200,
             'message' => 'Lấy trạng thái yêu thích thành công',
             'is_favorited' => $isFavorited
-        ]);
-    }
-
-    /**
-     * Get all favorited products for the authenticated user.
-     */
-    public function index()
-    {
-        $user = Auth::user();
-        $favoritedProducts = $user->favorites()->get(); // Lấy tất cả sản phẩm mà user đã yêu thích
-        return response()->json([
-            'status' => 200,
-            'message' => 'Lấy danh sách sản phẩm yêu thích thành công',
-            'favorited_products' => $favoritedProducts
         ]);
     }
 }
